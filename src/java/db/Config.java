585f1f07fe08f27ff7e6d2e72f900f1739e6ee3e
Mohammad Dehghan <badil2013@hotmail.com>
2017-08-08 03:00:41 +0430

    Remove default DB credentials

--- src/java/db/Config.java
+++ src/java/db/Config.java
@@ -6,8 +6,8 @@ package db;
  */
 public class Config {
 
-    static String DB_USER = "root";
-    static String DB_PASSWORD = "ahmad17290";
+    static String DB_USER = "user";
+    static String DB_PASSWORD = "password";
     static String DB_DATABASE = "divice";
     static String DB_SERVER = "jdbc:mysql://localhost/";
     static String DB_PORT = "1527";
